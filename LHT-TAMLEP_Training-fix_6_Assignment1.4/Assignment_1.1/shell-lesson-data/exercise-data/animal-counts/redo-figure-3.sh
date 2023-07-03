6fd1357ea92bd6a9e0dfb115ee4c4cfd2178a220
nishant-wadhwani-ta <nishant.wadhwan@tigeranalytics.com>
2023-07-03 18:45:21 +0530

    Assignment_1.4 files added

---
+++ LHT-TAMLEP_Training-fix_6_Assignment1.4/Assignment_1.1/shell-lesson-data/exercise-data/animal-counts/redo-figure-3.sh
@@ -0,0 +1,5 @@
+  913  cut -d, -f 2 animals.csv | sort | unique
+  914  cut -d, -f 2 animals.csv | sort | uniq
+  915  nano species.sh
+  916  bash species.sh animals.csv
+  917  history | tail -n 5 > redo-figure-3.sh
